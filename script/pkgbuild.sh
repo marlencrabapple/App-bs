26202ff59179a2419898a4899cc856be1c213c64
Ian P Bradley <ian.bradley@studiocrabapple.com>
2025-05-31 21:23:01 -0700

    Checking in changes prior to tagging of version 0.
    Changelog diff is:
    
    diff --git a/Changes b/Changes
    index bf9b6d5..1dfe84e 100644
    --- a/Changes
    +++ b/Changes
    @@ -2,7 +2,10 @@ Revision history for Perl extension App-BS
    
     {{$NEXT}}
         -
    -0 2025-06-01T04:17:38Z
    
    +    - not so first version
    +    - port aurdit code to Perl and rewrite most existing shell

--- script/pkgbuild.sh
+++ script/pkgbuild.sh
@@ -1,10 +1,16 @@
 #!/usr/bin/env bash
-scriptdir="${0//\/$(basename "$0")}"
-. "$scriptdir/bs-common.sh"
 
+scriptdir="${0//\/$(basename "$0")/}"
+if [[ -f "$scriptdir/bs-common.sh" ]]; then
+  . "$scriptdir/bs-common.sh"
+else
+  echo "Error: Required file bs-common.sh not found in $scriptdir" >&2
+  exit 1
+fi
 
 [[ "${DEBUG:=0}" -eq 1 ]] && set -x
 
+startdir="$(pwd)"
 arch_packaging_repo_base="https://gitlab.archlinux.org/archlinux/packaging/packages"
 
 pkgbuild_dir="${BS_ROOT:=/bs}/pkgbuild"
@@ -14,11 +20,15 @@ logdir="${BS_ROOT:=/bs}/log"
 
 fetch_aur_pkg() {
   pkgbase="$1"
-  out="$(plenv shell system &&
-    aur fetch -r "$pkgbase")"
+  PLENV_VERSION=system
+
+  out="$(
+    env PLENV_VERSION=system \
+      aur fetch -r "$pkgbase"
+  )"
   echo "$out"
 
-  return "${out[-1]}"
+  return "${out[*]:-1:1}"
 }
 
 get_update_pkgbuild() {
@@ -27,7 +37,7 @@ get_update_pkgbuild() {
 
   if [[ ! -d "$pkgbase" ]]; then
     git clone "$arch_packaging_repo_base/$pkgbase.git"
-    err="$?"
+    err="${?:-0}"
 
     [[ "${err:-0}" -eq 0 ]] && return 0
 
@@ -40,7 +50,7 @@ get_update_pkgbuild() {
 
       for repo in "${BS_REPOS[@]}"; do
         git clone "$BS_USERREPO_BASE_URI/$repo/$pkgbase.git"
-        err="$?"
+        err="${?:-0}"
 
         if [[ "${err:-0}" -ne 0 ]]; then
           warn "Failed to clone '$pkgbase' from user added repo '$repo"
@@ -57,24 +67,27 @@ expac_query_dbs() {
   pkgchoices=()
 
   for db in Q S "${userdb[@]}"; do
-    pkgchoices+=($(expac "-${db}s" '%r/%e' "$pkgstr"))
+    pkgchoices+=("$(expac "-${db}s" '%r\/%e' $pkgstr)")
   done
+
+  echo "${pkgchoices[@]}"
+  return ${?:-0}
 }
 
 package_choice() {
-  pkgchoices=("$@")
+  pkgchoices=($@)
+  first="${pkgchoices[*]:-1:0}"
 
   choice=(
-    "${pkgchoices[*]:0:1//\/*/}"
-    "${pkgchoices[*]:0:1//*\//}"
+    "${first//\/*/}"
+    "${first//*\//}"
   )
 
   [[ ${DEBUG:-0} -ne 0 ]] && warn "pkgchoices: ${pkgchoices[*]}"
   [[ ${DEBUG:-0} -ne 0 ]] && warn "choice: ${choice[*]}"
 
   echo "${choice[@]}"
-  return $?
-
+  return ${?:-0}
 
   #local i=0
   #for pkgrepo in "${pkgchoices[@]}"; do
@@ -84,58 +97,89 @@ package_choice() {
   #echo ${pkgchoices[*]:0:1}
 }
 
+enter_pkgbuild_repo() {
+  pkgbase="$1"
+  cd "${pkgchoice[*]:-1:0}" || return $?
+  get_update_pkgbuild "${pkgchoice[@]}"
+
+  branches=($(git branch --all))
+  curr_branch="${branches[*]:0:1}"
+  new_branch="$curr_branch-$(date +%s)"
+
+  git switch -c "$new_branch"
+  git add -A
+  git commit -S -m "Unsynced changes pre-rebase and rebuild"
+  git push "$new_branch"
+
+  for branch in "${branches[@]}"; do
+    [[ -z "${branch//main|master/}" ]] || continue
+
+    git switch "$curr_branch"
+    git pull "$branch" --rebase
+    err="${?:-0}"
+
+    while [[ "${err:-0}" -ne 0 ]]; do
+      git mergetool
+      err="${?:-0}"
+      git rebase --continue
+      err="${?:-0}"
+    done
+
+    git push "$curr_branch"
+  done
+}
+
+do_makechrootpkg() {
+  pkgbase="$1"
+  [[ -z "$pkgbase" ]] && warn "No pkgbase provided." && return 1
+  cleanchroot="${2:=0}"
+  cleanbuilddir="${3:=0}"
+
+  local makechrootpkg_opts=(makechrootpkg
+    -Cun${cleanchroot:+c}
+    -r"$CHROOT" - -sifAL${cleanbuilddir:+Cc})
+
+  say "$("${makechrootpkg_opts[@]}")"
+  return "${?:-0}"
+}
+
+do_bsrepoadd() {
+  pkgbase="$1"
+
+  local bsrepoadd_opts=(
+    env BS_CLOBBER=1
+    bs-repoadd "$PKGDEST/$pkgbase"
+  )
+
+  say "$("${bsrepoadd_opts[@]}")"
+}
+
 handle_pkgspec() {
   local pkgspec="$1"
 
   # FIX ME: First result is probably what we want unless the user declares
   # otherwise in the current local git config or a bs-repo-conf.toml file in
   # the repo root
-  pkgchoices=$(expac_query_dbs "$pkgspec")
-  pkgrepo=("${pkgchoices[*]:0:1}")
-  pkgbase=("${pkgchoices[*]:0:2}")
+  pkgchoices=($(expac_query_dbs "$pkgspec"))
+  pkgchoice=($(package_choice "${pkgchoices[@]}"))
 
   # Fairly sure pactree includes the provided pkgspec compliant string in the
   # results...
-  pkgtree=($(pactree -lus "$pkgbase"))
+  pkgtree=($(pactree -lus "${pkgchoice[*]:-1:0}"))
 
-  for pkgstr in "${pkgtree[@]}"; do
-    local pkgchoices=$(expac_query_dbs "$pkgspec")
-
-    local pkgrepo=("${pkgchoices[*]:0:1}")
-    local pkgbase=("${pkgchoices[*]:0:2}")
-
-    #cd "$pkg" || continue # Superflous directory check
-    get_update_pkgbuild "$pkgrepo" "$pkgbase"
-
-    branches=($(git branch --all))
-    curr_branch="${branches[*]:0:1}"
-    new_branch="$curr_branch-$(date +%s)"
-
-    git switch -c "$new_branch"
-    git add -A
-    git commit -S -m "Unsynced changes pre-rebase and rebuild"
-    git push "$new_branch"
-
-    for branch in "${branches[@]}"; do
-      [[ -z "${branch//main|master/}" ]] || continue
-
-      git switch "$curr_branch"
-      git pull "$branch" --rebase
-      err="$?"
-
-      while [[ "${err:-0}" -ne 0 ]]; do
-        git mergetool
-        err="$?"
-        git rebase --continue
-        err="$?"
-      done
-
-      git push "$curr_branch"
-    done
-    cd "$pkgbuild_dir" || warn "Failed to change directory"
+  for dep_pkgspec in "${pkgtree[@]}"; do
+    handle_pkgspec "$dep_pkgspec"
   done
+
+  enter_pkgbuild_repo "${makechrootpkg_opts[@]}"
+  local err="${?:-0}"
+
+  [[ ${err:-0} -ne 0 ]] && $("${bsrepoadd_opts[@]}")
 }
 
 for pkgspec in "$@"; do
+  cd "$pkgbuild_dir" || die "Failed to change back to PKGBUILD directory" $?
   handle_pkgspec "$pkgspec"
 done
+
+cd "$startdir" || die "Failed to change back to starting directory" $?
