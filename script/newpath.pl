cf1201a5d5e8f677e606a356d99f92e270dc0351
Ian P Bradley <ian.bradley@studiocrabapple.com>
2025-10-18 22:27:44 -0700

    ...

--- script/newpath.pl
+++ script/newpath.pl
@@ -1,35 +1,36 @@
 #!/usr/bin/env perl
 use Object::Pad ':experimental(:all)';
 
+# TODO: CLosing handles with defer, DESTROY block
+package BS::Path;
+use lib 'lib';
+
+class BS::Path : does(BS::Common);
+field $handle;
+
 use utf8;
 use v5.40;
 
-use lib 'lib';
-
 use BS::Common;
-use Digest::SHA qw(sha1_hex sha256_hex sha512_hex)
+use Digest::SHA qw(sha1_hex sha256_hex sha512_hex);
+field $pathto;
 
-# TODO: CLosing handles with defer, DESTROY block
-
-class BS::Path :does(BS::Common) {
-  field $handle;
-  field $pathto;
-  # v1: common method and AUTOLOAD or symbol table hacking
-  # v2: subroutine
-  method $_path ($pathto, %opts) {
-  $mode = $opts{mode} // '<';
-  state %handles = ();
-  open my $fh, $mode, $pathto or die "❌️ Could not open '$pathto' (mode: $mode)"
-  $handles{$fh} = $fh;
-  BS::Common::dmsg({ handles => \%handles })
+# v1: common method and AUTOLOAD or symbol table hacking
+# v2: subroutine
+method $_path ( $pathto, %opts ) {
+    my $mode = $opts{mode} // '<';
+    state %handles = ();
+    open my $fh, $mode, $pathto
+      or BS::Common::fatal("Could not open '$pathto' (mode: $mode)");
+    $handles{$fh} = $fh;
+    BS::Common::dmsg( { handles => \%handles } );
 }
 
-sub path :
+#sub path {
+#
+#}
 
-  method path :common () {
-
-  }
-
+method path : common () {
 
 }
 
