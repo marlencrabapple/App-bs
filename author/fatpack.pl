40b411ac42af8158133dd127807c74134bb5ae5b
Ian P Bradley <ian.bradley@studiocrabapple.com>
2026-04-13 04:49:02 -0700

    ...

--- author/fatpack.pl
+++ author/fatpack.pl
@@ -5,34 +5,77 @@ use v5.40;
 
 use lib 'lib';
 
-use Cwd;
+use Cwd 'abs_path';
 use File::chdir;
 use Path::Tiny;
-use Getopt::Long
-qw(GetOptionsFromArray :config no_ignore_case bundling auto_abbrev);
+use Getopt::Long qw(GetOptionsFromArray :config no_ignore_case auto_abbrev);
 
-use IPC::Nosh 'run';
+use IPC::Nosh;
 use IPC::Nosh::IO;
 
-our $modroot  = path('./')->absolute;
-our $input    = path("$modroot/script");
-our $outdir   = path( "$modroot/fatpackout." . time );
-our $outfn    = "%s.fat";
+our $modroot  = path(abs_path);
+our @input    = ( path("$modroot/script") );
+our $outdir   = path('./bin');
+our $outfn    = '%s';
 our $locallib = path("$modroot/local");
 our $verbose  = 1;
 our $debug    = $verbose;
 
-dmsg( $ENV{PERL5LIB} );
+our $patharg = sub ( $arg, %opt ) {
+    $arg = path($arg)->assert(
+        sub {
+            $opt{assert} && $opt{assert} isa CODE ? $opt{assert}->(@_) : 1;
+        }
+    ) unless $arg isa Path::Tiny;
+
+    if ( my $dest = $opt{dest} ) {
+        if ( my $type = ref $dest ) {
+            if ( $type eq 'ARRAY' ) {
+                push @$dest, $arg;
+            }
+            elsif ( $type eq 'SCALAR' ) {
+                $$dest = $arg;
+            }
+        }
+        else {
+            fatal '$dest must be a SCALAR, ARRAY, or CODE reference!';
+            dmsg( $arg, $dest );
+        }
+    }
+};
+
+our %clidest = (
+    modroot => \$modroot,
+    input   => sub {
+        $patharg->( shift, assert => sub { shift->exists } );
+    },
+    outdir   => \$outdir,
+    outfn    => \$outfn,
+    locallib => \$locallib,
+    verbose  => \$verbose,
+    debug    => \$debug
+);
 
 GetOptions(
-    'input|file|script|=s',
+    \%clidest,
+    'input|file|infile|infname|script=s{,}',
     'outdir|fatpack-out=s',
-    'outfilename|outfn|fnfmt|fmtfn|fmt-filename|fmt-outputfn=s',
+    'outfn|outfname|out-filename|fnfmt|fmtfn|fmt-filename|fmt-outputfn=s',
     'modroot|module-root|module-dir=s',
-    'locallib=s',
+    'locallib=s{,}',
     'verbose+',
-    'debug'
+    'debug',
+    '<>' => sub ($in) { push @input, $patharg->($in) }
 );
+
+my $_cliopt = {
+    map {
+        my $ref = ref $clidest{$_};
+        ( $_ => ( $ref eq 'SCALAR' ? $clidest{$_}->$* : $clidest{$_} ) )
+    } ( keys %clidest )
+};
+
+dmsg($_cliopt);
 
 sub fatpack {
     $CWD = $modroot;
@@ -45,12 +88,8 @@ sub fatpack {
 
     $outdir->mkdir unless -d $outdir;
 
-    foreach my $in (
-          $input->is_dir     ? $input->children
-        : $input isa 'ARRAY' ? @$input
-        :                      $input
-      )
-    {
+    foreach my $in ( map { $_->is_dir ? ( $_->children ) : $_ } @input ) {
+
         #fatpack($in->children) if $in->is_dir;
         my @fatlines;
         my $fatstr = "";
@@ -60,16 +99,16 @@ sub fatpack {
         info( "Running " . join " ", @cmd );
 
         run( \@cmd, out => \@fatlines, autoflush => 1, autochomp => 1 );
-        
+
         $fatstr = join "\n", @fatlines;
 
-        my $fatout = sprintf( ( $outfn || '%s.fat' ), $in->basename );
+        my $fatout = sprintf(
+            ( $outfn || '%s.fat' ),
+            ( s/^(.+)(?:\.pl)?$/$1/rg =~ $in->basename )
+        );
 
-        if ( my $ext = $in->basename =~ /\.(pl)$/i ) {
+        if ( my ($ext) = $in->basename =~ /\.(pl)$/i ) {
             $fatout .= ".$ext";
-        }
-        else {
-            $fatout .= ".pl";
         }
 
         path("$outdir/$fatout")->spew_utf8($fatstr);
